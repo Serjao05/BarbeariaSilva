c1aff49c0d73ff7dfe6c2368ac0ff85fa8e06238
sergioSouza <sergio_souza.s@hotmail.com>
2025-11-17 21:06:43 -0300

    Alteração HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics;
 using BarbeariaSilva.Models;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Generators;
+using BCrypt.Net;
+using Npgsql;
 
 namespace BarbeariaSilva.Controllers
 {
@@ -24,11 +27,103 @@ namespace BarbeariaSilva.Controllers
             return View("~/Views/Cliente/Cadastro.cshtml");
         }
 
+        [HttpPost]
+
+        public IActionResult CadastroCliente(ClienteViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Id = Guid.NewGuid();
+
+                    string senhaCriptografada = BCrypt.Net.BCrypt.HashPassword(model.Senha);
+
+                    var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+
+                    const string query = @"
+                INSERT INTO cliente (id, nome, email, senha)
+                VALUES (@id, @nome, @email, @senha);
+                    ";
+
+                    using (var connection = new NpgsqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        using (var cmd = new NpgsqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("id", model.Id);
+                            cmd.Parameters.AddWithValue("nome", model.Nome ?? string.Empty);
+                            cmd.Parameters.AddWithValue("email", model.Email ?? string.Empty);
+                            cmd.Parameters.AddWithValue("senha", senhaCriptografada ?? string.Empty);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    return View("~/Views/Home/Login.cshtml");
+                }
+                catch (Exception ex) {
+                    Console.WriteLine($"Erro ao registrar o Cliente: {ex.Message}");
+
+                    ModelState.AddModelError(string.Empty, "Ocorreu um erro ao registraro Cliente. Tente Novamente.");
+                }
+            }
+
+            return Error();
+        }
+
         [HttpGet]
         public IActionResult CadastroBarbeiro()
         {
             return View("~/Views/Barbeiro/Cadastro.cshtml");
         }
+
+        [HttpPost]
+
+        public IActionResult CadastroBarbeiro(BarbeiroViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Id = Guid.NewGuid();
+
+                    string senhaCriptografada = BCrypt.Net.BCrypt.HashPassword(model.Senha);
+
+                    var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+
+                    const string query = @"
+                INSERT INTO barbeiro (id, nome, email, senha)
+                VALUES (@id, @nome, @email, @senha);
+                    ";
+
+                    using (var connection = new NpgsqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        using (var cmd = new NpgsqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("id", model.Id);
+                            cmd.Parameters.AddWithValue("nome", model.Nome ?? string.Empty);
+                            cmd.Parameters.AddWithValue("email", model.Email ?? string.Empty);
+                            cmd.Parameters.AddWithValue("senha", senhaCriptografada ?? string.Empty);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    return View("~/Views/Home/Login.cshtml");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao registrar o Barbeiro: {ex.Message}");
+
+                    ModelState.AddModelError(string.Empty, "Ocorreu um erro ao registraro Barbeiro. Tente Novamente.");
+                }
+            }
+
+            return Error();
+        }
+
 
         public IActionResult Privacy()
         {
--- Models/ClienteViewModel.cs
+++ Models/ClienteViewModel.cs
@@ -8,21 +8,25 @@ namespace BarbeariaSilva.Models
 {
     public class ClienteViewModel
     {
+
+        public Guid Id { get; set; }
+
+
         [Required(ErrorMessage = "O Nome é obrigatório.")]
         [StringLength(100, ErrorMessage = "O Nome não pode exceder 100 caracteres.")]
 
-        public string? nome { get; set; }
+        public string? Nome { get; set; }
 
         [Required(ErrorMessage = "O E-mail é obrigatório.")]
         [EmailAddress(ErrorMessage = "O Formato do E-mail não é válido.")]
 
-        public string? email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "A senha é obrigatória.")]
         [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
         [DataType(DataType.Password)]
 
-        public string? senha { get; set; }
+        public string? Senha { get; set; }
 
         [Compare("senha", ErrorMessage = "As senhas não coincidem.")]
         [DataType(DataType.Password)]
